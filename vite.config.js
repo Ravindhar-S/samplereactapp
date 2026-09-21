eb7c7a0556e85a1afae233c7e7371b17739d63ef
Ravindhar-S <ravin295@gmail.com>
2026-09-20 22:44:16 -0400

    Code repo deploy test

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    server: {
+        watch: {
+            // Tells Vite's file watcher to completely ignore Visual Studio's hidden data folder
+            ignored: ['**/.vs/**']
+        }
+    }
 })
